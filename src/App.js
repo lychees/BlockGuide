c06784615dc0e8a950daf39434af533f7f04b754
Dafeng Guo <dfguo.joe@gmail.com>
2017-11-25 00:33:34 +0800

    add comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
@@ -8,14 +8,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Block Guide</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">TechCrunch Shanghai 2017</p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
