16a1e934fce38bc576aaaf2c1def344ecd900c76
Dafeng Guo <dfguo.joe@gmail.com>
2017-11-26 07:12:04 +0800

    add rating

--- mini/pages/purchase/rate.js
+++ mini/pages/purchase/rate.js
@@ -2,24 +2,30 @@ Page({
   data: {
     comment: '',
   },
-  onLoad: function (e) {
-
-  },
-  bindInputChange: function (e) {
-    this.setData({comment: e.detail.value});
+  onLoad: function(e) {},
+  bindInputChange: function(e) {
+    this.setData({ comment: e.detail.value })
   },
   bindSubmit(e) {
     if (this.data.comment) {
+      const waitTime = Math.floor(Math.random() * 5000) + 5 * 1000
       wx.showToast({
-        title: '评价成功',
-        icon: 'success',
-        duration: 3000,
-        success: () => {
-          setTimeout(() => {
-            wx.switchTab({ url: '/pages/index/index' });
-          }, 1500);
-        }
-      }); 
+        title: '正在入链',
+        icon: 'loading',
+        duration: waitTime,
+      })
+      setTimeout(() => {
+        wx.showToast({
+          title: '评价成功',
+          icon: 'success',
+          duration: 3000,
+          success: () => {
+            setTimeout(() => {
+              wx.switchTab({ url: '/pages/index/index' })
+            }, 1500)
+          },
+        })
+      }, waitTime)
     }
-  }
+  },
 })
