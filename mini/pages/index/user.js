48a98abedd0682e864e8cadb3d10457563767987
TennnyZhuang <zty0826@gmail.com>
2017-11-26 07:02:20 +0800

    show credit in user

--- mini/pages/index/user.js
+++ mini/pages/index/user.js
@@ -1,1 +1,17 @@
-Page({})
+Page({
+  data: {
+    balance: 0,
+  },
+  onLoad(e) {
+    let balance = wx.getStorageSync('balance');
+    if (!balance) {
+      balance = 0.2 + Math.random() * 0.8;
+      balance = parseInt(balance * 100) / 100;
+      wx.setStorageSync('balance', balance);
+    }
+
+    this.setData({
+      balance: balance,
+    });
+  }
+})
