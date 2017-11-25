fc437c4bf4ae247ca4590c39128c6f441b2449aa
TennnyZhuang <zty0826@gmail.com>
2017-11-25 15:50:18 +0800

    shop init

---
+++ mini/pages/shop/shop.js
@@ -0,0 +1,19 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    shopInfo: {
+      name: '鸟藏日料',
+      headerUrl: 'https://i.loli.net/2017/11/25/5a191f6048b55.jpg',
+      location: '地址',
+      phone: '021-xxxxxxx',
+      bussinessTime: '10:00 - 22:00',
+      description: '上海市第一家做河豚鱼精食的店',
+    },
+  },
+  onLoad: function () {
+
+  }
+})
